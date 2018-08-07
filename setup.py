6cdc2384156bc507c36b99503d33877f26826e99
CyberZHG <CyberZHG@gmail.com>
2018-08-07 08:30:12 +0800

    Update license in setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='keras-bi-lm',
-    version='0.0.6',
+    version='0.0.7',
     packages=['keras_bi_lm'],
     url='https://github.com/PoWWoP/keras_bi_lm',
-    license='LICENSE',
+    license='MIT',
     author='CyberZHG',
     author_email='CyberZHG@gmail.com',
     description='Train the Bi-LM model and use it as a feature extraction method',
@@ -15,6 +15,7 @@ setup(
         'keras',
     ],
     classifiers=(
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
