0aa9aa9e73dafb328dc1bc58ccc82ef4bcf1d668
CyberZHG <CyberZHG@gmail.com>
2018-08-17 21:44:24 +0800

    Fix demo

--- demo/sentiment_analysis.py
+++ demo/sentiment_analysis.py
@@ -192,11 +192,11 @@ predicts = model.predict_generator(
 )
 predicts = numpy.argmax(predicts, axis=-1).tolist()
 correct = 0
-for i in range(len(predicts)):
-    if i % batch_size < batch_size:
+for i, predict in enumerate(predicts):
+    if i % batch_size < batch_size // 2:
         expect = 1
     else:
         expect = 0
-    if predicts[i] == expect:
+    if predict == expect:
         correct += 1
 print('Accuracy: %.3f' % (1.0 * correct / len(predicts)))
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='keras-bi-lm',
-    version='0.0.16',
+    version='0.0.17',
     packages=['keras_bi_lm'],
     url='https://github.com/CyberZHG/keras-bi-lm',
     license='MIT',
