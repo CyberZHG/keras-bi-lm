9cb5c25344f08298991a03d56af201b899480f02
CyberZHG <CyberZHG@gmail.com>
2019-01-31 08:50:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
-from setuptools import setup
+import codecs
+from setuptools import setup, find_packages
+
+
+with codecs.open('README.md', 'r', 'utf8') as reader:
+    long_description = reader.read()
+
+
+with codecs.open('requirements.txt', 'r', 'utf8') as reader:
+    install_requires = list(map(lambda x: x.strip(), reader.readlines()))
+
 
 setup(
     name='keras-bi-lm',
-    version='0.21.0',
-    packages=['keras_bi_lm'],
+    version='0.22.0',
+    packages=find_packages(),
     url='https://github.com/CyberZHG/keras-bi-lm',
     license='MIT',
     author='CyberZHG',
     author_email='CyberZHG@gmail.com',
     description='Train the Bi-LM model and use it as a feature extraction method',
-    long_description=open('README.rst', 'r').read(),
-    install_requires=[
-        'numpy',
-        'keras',
-    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=install_requires,
     classifiers=(
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 3.7",
